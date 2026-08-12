114a878a6d64476c6af9ee682563800cd8ec5a0f
nikhildike22 <nikhildike22@gmail.com>
2026-08-12 19:33:33 +0530

    vercel config

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+     baseURL: "", // this will be replaced by the proxy in vite.config.js and for local put localhost:3000
     withCredentials: true,
 })
 
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   proxy: {
+      // forward requests starting with /api to the backend
+      "/api": {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path ,// keep /api/v1/users/register intact
+      }
+    }
+
 })
